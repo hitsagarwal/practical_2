c115a9065207b0e1401e505ffc3f7396207192bf
hitsagarwal <96682132+hitsagarwal@users.noreply.github.com>
2022-04-02 21:23:25 +0530

    commit practical_2

---
+++ practical_2/src/practical_2/p02_task01_keywordsinjava.java
@@ -0,0 +1,25 @@
+package practical_2;
+import java.util.*;
+
+public class p02_task01_keywordsinjava {
+    public static void main(String[] args) {
+        float float =2;
+       // Syntax error on token "float", invalid VariableDeclaratorIdJava(1610612971)
+        float();
+    }
+
+    Static void float()
+    {   // Syntax error on tokens, delete these tokensJava(1610612969)
+        System.out.println("hello");
+    }
+
+    abc static = new abc();
+    //Syntax error on token "static", invalid VariableDeclaratorIdJava(1610612971)
+}
+class abc
+{
+    int print()
+    {
+        System.out.println("hello");
+    }
+}
