83642e95171730e9c913ae7a8a89cd30cde71770
hitsagarwal <96682132+hitsagarwal@users.noreply.github.com>
2022-04-03 10:05:13 +0530

    commit constant in java

---
+++ practical_2/src/practical_2/p02_task02_constantsinjava.java
@@ -0,0 +1,14 @@
+package practical_2;
+import java.util.*;
+public class p02_task02_constantsinjava {
+	
+	    static final float pi = 3.14f; // global constant
+
+	    public static void main(String[] args) {
+	        final int r = 10; 
+	        float area = pi * r * r;
+	        System.out.println(area);
+	    }
+
+	}
+}
